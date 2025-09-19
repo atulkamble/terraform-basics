f8119afb01a9a2a57ee4285f896cdec6b6a841b9
Atul Kamble <atul_kamble@hotmail.com>
2025-09-19 10:49:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ provider "aws" {
 }
 
 resource "aws_instance" "my_ec2" {
-  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2
-  instance_type = "t2.micro"
+  ami           = "ami-08982f1c5bf93d976" # Amazon Linux 2023
+  instance_type = "t3.micro"
 
   tags = {
     Name = "MyFirstEC2"
